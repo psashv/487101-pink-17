f846db2dbb61ef6733ecd91c4f278dfd1a8edb51
xAnderoov <psashv@gmail.com>
2020-01-02 15:44:08 +0300

    оживляет навигацию

---
+++ source/js/nav.js
@@ -0,0 +1,15 @@
+var siteHeader = document.querySelector('.site-header');
+var navSite = document.querySelector('.site-nav');
+var navToggle = document.querySelector('.site-nav__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navSite.classList.contains('site-nav--closed')) {
+    navSite.classList.remove('site-nav--closed');
+    siteHeader.classList.remove('site-header--closed');
+    navSite.classList.add('site-nav--opened');
+  } else {
+    navSite.classList.add('site-nav--closed');
+    navSite.classList.remove('site-nav--opened');
+    siteHeader.classList.add('site-header--closed');
+  }
+});
